244041de9c68b37515d648e266093dc079774cb1
Tong Wu <Tong@tongs-mbp.dhcp.nd.edu>
2017-06-18 16:58:33 -0400

    write to csv file

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+#write to csv file
+
+import csv
+
+emaildata = []
+
+with open ('faculty.csv') as csvfile:
+    data = csv.reader(csvfile)
+    for row in data:
+        emaildata.append(row[3])
+csvfile.close()
+
+emaildata.pop(0)
+with open ('emails.csv','w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    for item in emaildata:
+        writer.writerow([item])
+csvfile.close()
