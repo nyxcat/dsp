b6869cc18640fb1a19c77b34f37cf5c9054732fd
Tong Wu <Tong@Tongs-MacBook-Pro.local>
2017-06-20 14:46:00 -0400

    markov text generator

--- python/markov.py
+++ python/markov.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python                                                                                             
+#!/usr/bin/env python          A                                                                                   
 
 # Write a Markov text generator, [markov.py](python/markov.py). Your program should be called from the command line with two arguments: the name of a file containing text to read, and the number of words to generate. For example, if `chains.txt` contains the short story by Frigyes Karinthy, we could run:                                   
 
@@ -15,64 +15,62 @@
 import numpy as np
 import sys
 import random
+import csv
+import re
 
 filename = sys.argv[1]
 wordnum = int(sys.argv[2])
 
 frequency = {}
 totalnum = {}
+beginfrequency = {}
+beginnum = 0
 
 with open(filename) as txtfile:
-    s = txtfile.read()
-    i = 0
-    while i < (len(s)-2):
-        print(i)
-        while s[i] == '\\' or s[i] == '\n':
-            i += 1
-            if i+2 >= len(s):
-                break
-        index_first = ord(s[i]) - ord(' ')
-        if (index_first < 0) or (index_first > 95):
-            i += 1
+    text = txtfile.read()
+    sentences = text.split('\n\n')
+    for sentence in sentences:
+        wordinsentence = sentence.split()
+        if len(wordinsentence) == 0:
             continue
-        while s[i+1] == '\\' or s[i+1] == '\n':
-            i += 1
-            if i+2 >= len(s):
-                break
-        index_second = ord(s[i+1]) - ord(' ')
-        if index_second < 0 or index_second > 95:
-            i += 1
-            continue
-        while s[i+2] == '\\' or s[i+1] == '\n':
-            i += 1
-            if i+2 >= len(s):
-                break
-        index_third = ord(s[i+2]) - ord(' ')
-        if index_third < 0 or index_third > 95:
-            i += 1
-            continue
-        if (s[index_first] + s[index_second]) in frequency:
-            frequency[s[index_first]+s[index_second]][index_third] += 1
-            totalnum[s[index_first]+s[index_second]] += 1
+        beginnum += 1
+        beginword = wordinsentence[0]
+        if beginword in beginfrequency:
+            beginfrequency[beginword] += 1
         else:
-            frequency[s[index_first]+s[index_second]] = np.zeros(95)
-            frequency[s[index_first]+s[index_second]][index_third] = 1
-            totalnum[s[index_first]+s[index_second]] = 1
-        i += 1
+            beginfrequency[beginword] = 1
+    words = re.split('\n| |\"|\'',text)
+    for i, word in enumerate(words):
+        if word == '':
+            words.pop(i)
+    for i, word in enumerate(words[:-2]):
+        if word in frequency:
+            totalnum[word] += 1
+            if words[i+1] in frequency[word]:
+                frequency[word][words[i+1]] += 1
+            else:
+                frequency[word][words[i+1]] = 1
+        else:
+            frequency[word] = {words[i+1]: 1}
+            totalnum[word] = 1
 txtfile.close()
 
-for key, value in totalnum.items():
-    frequency[key] = frequency[key] / value
-print(totalnum['. '])
-'''count = 0
-string = ''
-string += chr(random.choices(range(0,95), frequency['  '])[0] + ord(' '))
-string += chr(random.choices(range(0,95), frequency[' '+string[0]])[0] + ord(' '))
-while count < wordnum:
-    c_firsttwo = string[-2:]
-    c = chr(random.choices(range(0,95), frequency[c_firsttwo])[0] + ord(' '))
-    string += c
-    if c == ' ':
-        count += 1
-print(string)
-'''
+for word, value in beginfrequency.items():
+    beginfrequency[word] = value/beginnum
+for word, totalnum in totalnum.items():
+    if totalnum != 0:
+        for nextword, num in frequency[word].items():
+            frequency[word][nextword] = num/totalnum
+
+preword = np.random.choice(list(beginfrequency.keys()),
+                           p = list(beginfrequency.values()))
+pseudotext = '' + preword
+
+for i in range(wordnum - 1):
+    pseudotext += ' '
+    preword = np.random.choice(list(frequency[preword].keys()),
+                                p = list(frequency[preword].values()))
+    pseudotext += preword
+print(pseudotext)
+ 
+
